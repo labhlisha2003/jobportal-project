f5b17be2d93ed37c18108a46606f02cedc40db56
komalthakurr <komalthakur00y@gmail.com>
2025-03-17 12:19:34 +0530

    firebase changes

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -1,30 +1,34 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./style.css";
 import { FaEyeSlash } from "react-icons/fa";
 import { FaEye } from "react-icons/fa";
 
-function CustomInput({type,placeholder,Icon,IconColor,isSecureEntry= false,inputValue,onChangeText, }) {
-    const [showText, setShowText]=useState(false);
-return(
+function CustomInput({ type, placeholder, Icon, IconColor, isSecureEntry = false, inputValue, onChangeText, }) {
+    const [showText, setShowText] = useState(false);
+    return (
         <div className="customInputBaseContainer">
             <div className="customInputIConContainer">
-                {Icon ?<Icon Color={ IconColor} sixe={19}/>:null}
-                </div>
-                <div className="customInputInputContainer">
-                    <input  value={inputValue} type={isSecureEntry ? (showText ? "text":"Password"): type} placeholder={placeholder }onChange={{onChangeText}}/></div>
-                    
-                    {isSecureEntry &&(
-                    <div className="customInputPasswordContainer"
-                    onClick={()=>{setShowText(!showText);
+                {Icon ? <Icon Color={IconColor} sixe={19} /> : null}
+            </div>
+            <div className="customInputInputContainer">
+                <input value={inputValue} type={isSecureEntry ? (showText ? "text" : "Password") : type} placeholder={placeholder} onChange={onChangeText} /></div>
+
+            {isSecureEntry && (
+                <div className="customInputPasswordContainer"
+                    onClick={() => {
+                        setShowText(!showText);
 
                     }}
-                    >{showText ?<FaEye/>:<FaEyeSlash/>}
+                >{showText ? <FaEye /> : <FaEyeSlash />}
+        
 
-                    </div>
-                    )}
-                
+                </div>
+            
+            )}
+
         </div>
-    
+        
+
     );
 }
 export default CustomInput;
