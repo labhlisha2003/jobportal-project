9b6321580386ce91b04172f0fa0e11eb66b328f5
labhlisha2003 <sainilovenisha@gmail.com>
2025-04-09 13:53:27 +0530

    changes done

--- src/Pages/HomePage/Pages/BlogPage/BlogPage.js
+++ src/Pages/HomePage/Pages/BlogPage/BlogPage.js
@@ -1,6 +1,72 @@
-import React from"react";
+import React , { useEffect, useState } from "react";
 import "./style.css";
-function BlogPage(){
-    return <div>BlogPage</div>;
-}
-export default BlogPage;
+import CustomButtom from"../../../../components/CustomButton/CustomButton";
+import COLORS from"../../../../config/color";
+import { useNavigate } from "react-router-dom";
+import CustomButton from "../../../../components/CustomButton/CustomButton";
+import { RotatingLines } from "react-loader-spinner";
+import COLOR from "../../../../config/color";
+import { database } from "../../../../firebase";
+import { get, child, ref } from "firebase/database";
+
+export default function BlogPage() {
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading]  = useState(true);
+    const [blogData, setBlogData] = useState([]);
+
+    useEffect(() => {
+        getDataOfBlog();
+    }, []);
+
+    const getDataOfBlog = async () => {
+        setIsLoading(true);
+        const dbRef = ref(database, "blogs");
+        const snapshot = await get(dbRef);
+        if (snapshot.exists()) {
+            setBlogData(Object.values(snapshot.val()));
+            setIsLoading(false);
+            console.log(snapshot.val());
+        } else {
+                setIsLoading(false);
+        setBlogData([]);
+        alert("No Data Found");
+    }
+};
+
+    return (
+         <div className="blogPageBaseContainer">
+            <div className="blogPageContentContainer"> 
+                {isLoading ? (
+                    <RotatingLines
+                    strokeWidth="2"
+                    width="50"
+                    strokeColor={COLOR.blackColor}
+                    />
+                ) : (
+                    <div className="blogPageContentBaseContainer">
+                    {blogData.length == 0 ? (
+                        <p>No Data Found</p>
+                    ) : (
+                        blogData.map((item) => {
+                            return (
+                                <div className="blogPageItemBaseContainer">
+                                    <h1>{item.title}</h1>
+                                    <p>{item.content}</p>
+                                    </div>
+                            );
+                        })
+                        )}
+                        </div>
+                        )}
+                        </div>
+           <div className="blogPageAddButtonContainer">
+                <CustomButton
+                backgroundColor={COLORS.baseColorDark}
+                title={"Add Blog"}
+                color={COLORS.whiteColor}
+                onClick={() => navigate("/new-blog")}
+                />
+            </div>
+         </div>
+        );
+    }
